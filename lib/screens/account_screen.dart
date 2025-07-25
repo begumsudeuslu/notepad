8d0353c5c08b295303b829f382419595fa5fe8f4
merveocal <ocamerve752@gmail.com>
2025-07-25 15:55:29 +0300

    we are continue  this is branch

---
+++ lib/screens/account_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Hesap Sayfası(Giriş Yapma/Kayıt Olma)",
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
---
+++ lib/screens/note_screen.dart
@@ -0,0 +1,13 @@
+// lib/screens/notes_screen.dart
+import 'package:flutter/material.dart';
+
+class NotesScreen extends StatelessWidget {
+  const NotesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Notlar Sayfası (Ana Ekran)', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
